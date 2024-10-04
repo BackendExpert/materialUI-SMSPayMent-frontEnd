eb4ddd86c045015558a55b4f395aca57f5e96dd5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-04 13:18:29 +0530

    Update

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -24,28 +24,32 @@ const SignIn = () => {
         console.log('Form Data:', SignInData);
     }
   return (
-    <div className='bg-gray-200/50 w-full min-h-screen md:py-[10%] py-[20%]'>
+    <div className='bg-[#6C63FF] w-full min-h-screen md:py-[10%] py-[20%]'>
         <div className="md:grid grid-cols-3 gap-3">
             <div className=""></div>
-            <div className="bg-white py-8 px-4 shadow-md rounded md:mx-0 mx-4">
-                <center>
-                    <img src={SchoolIMG} alt="" className='h-40 w-auto'/>
-                    <p className="text-[#6C63FF] uppercase text-xl font-semibold">Signin Here</p>
-                </center>
+            <div className="">
+                <div className="bg-white py-8 px-4 shadow-md rounded md:mx-0 mx-4">
+                    <center>
+                        <img src={SchoolIMG} alt="" className='h-40 w-auto'/>
+                        <p className="text-[#6C63FF] uppercase text-xl font-semibold">Signin Here</p>
+                    </center>
 
-                <form onSubmit={headleSignIn} method="post" className='my-6 mx-4'>
-                    <div className="my-4">
-                        <TextFeildInput name={'email'} InputType={"email"} PlaceText={"Email Address"} TextColor={"#6C63FF"} BorderColor={"#6C63FF"} value={SignInData.email} onChange={handleChange}/>
-                    </div>
-                    <div className="my-4">
-                        <TextFeildInput name={'password'} InputType={"password"} PlaceText={"Password"} TextColor={"#6C63FF"} BorderColor={"#6C63FF"} value={SignInData.password} onChange={handleChange}/>
-                    </div>
+                    <form onSubmit={headleSignIn} method="post" className='my-6 mx-4'>
+                        <div className="my-4">
+                            <TextFeildInput name={'email'} InputType={"email"} PlaceText={"Email Address"} TextColor={"#6C63FF"} BorderColor={"#6C63FF"} value={SignInData.email} onChange={handleChange}/>
+                        </div>
+                        <div className="my-4">
+                            <TextFeildInput name={'password'} InputType={"password"} PlaceText={"Password"} TextColor={"#6C63FF"} BorderColor={"#6C63FF"} value={SignInData.password} onChange={handleChange}/>
+                        </div>
 
-                    <FormButton ButtonText={"SignIn"} TypeButton={"submit"}/>                    
-                </form>
+                        <FormButton ButtonText={"SignIn"} TypeButton={"submit"}/>                    
+                    </form>                 
+                </div>
+                <h1 className="text-center pt-2 font-semibold text-white uppercase text-sm">&copy; Developed and Design by JehnaKandy</h1>
             </div>
             <div className=""></div>
         </div>
+        
     </div>
   )
 }
